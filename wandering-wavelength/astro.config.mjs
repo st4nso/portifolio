c813daed6fcb16b16a937e2adbc30ab5d0ca0957
Lucas Stanso <st4nso@gmail.com>
2026-08-19 20:13:35 -0300

    Ajusta base URL para deploy

--- wandering-wavelength/astro.config.mjs
+++ wandering-wavelength/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://stanso.github.io',
-  base: '/',
+  site: 'https://st4nso.github.io',
+  base: '/photo-portifolio/',
 });
